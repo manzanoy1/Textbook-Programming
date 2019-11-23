56d701ac93fc32cc9ec21112a7b00dc539c210cc
Yanira Manzano <56289361+manzanoy1@users.noreply.github.com>
2019-11-22 22:13:41 -0500

    Create textbooks.py

---
+++ textbooks.py
@@ -0,0 +1,19 @@
+#Yanira Manzano
+#TP Assignment
+#22/11/2019
+
+from Person import Person
+
+class Textbooks():
+    def __init__(self, title, first, last, age, edition, ISBN, publisher, year, inventory, price):
+        self.title = title
+        self.author = Person(first, last, age)
+        self.edition = edition
+        self.ISBN = ISBN
+        self.publisher = publisher
+        self.year = year
+        self.inventory = inventory
+        self.price = price
+
+    def add_to_inventory(self, qty):
+        self.inventory = self.inventory + qty
