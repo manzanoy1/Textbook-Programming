e6d4884ea12610141fed535343012540f371e0be
Yanira Manzano <56289361+manzanoy1@users.noreply.github.com>
2019-11-22 22:12:38 -0500

    Create person.py

---
+++ person.py
@@ -0,0 +1,9 @@
+#Yanira Manzano
+#TP Assignment 
+#22/11/2019
+
+class Person:
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
